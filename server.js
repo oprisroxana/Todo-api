32e89143c96c3645ae81995204876084d3fe7335
Roxana Opris <dev.roxana@gmail.com>
2016-07-01 16:18:37 +0300

    Add completed filter

--- server.js
+++ server.js
@@ -15,12 +15,21 @@ app.get('/', function(req, res){
 
 // GET /todos
 app.get('/todos', function(req, res){
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+   if (queryParams.hasOwnProperty('completed') &&  queryParams.completed === "true") {
+     filteredTodos = _.where(filteredTodos, {completed : true });
+   } else if (queryParams.hasOwnProperty('completed') &&  queryParams.completed === "false"){
+     filteredTodos = _.where(filteredTodos, {completed : false });
+   }
+
+  res.json(filteredTodos);
 });
 
 //GET todos/:id
 app.get('/todos/:id', function(req, res){
-  var todoId =parseInt(req.params.id, 10) ;
+  var todoId = parseInt(req.params.id, 10) ;
   var match =  _.findWhere(todos, {id :todoId});
      if (match){
        res.json(match);
